67316ffaaa40a5626b3949468c68ce530ade23a3
Ryan Linton <ryanlntn@gmail.com>
2013-04-12 08:35:40 -0700

    add chapter 1

---
+++ chapter1.js
@@ -0,0 +1,38 @@
+// Chapter 1
+// What is JavaScript?
+
+// History
+  // JavaScript was created by Brendan Eich at Netscape.
+  // Soon after introducing it Microsoft released their own version.
+  // This caused all sorts of problems so it was decided that they
+  // would standardize.
+
+  // Long story short, we now have ECMAScript 5.
+
+// Implementations
+  // JavaScript consists of three main parts:
+    // 1. The ECMAScript Core Language
+    // 2. The Document Object Model (DOM)
+    // 3. The Browser Object Model (BOM)
+
+  // ECMAScript describes the:
+    // ➤ Syntax
+    // ➤ Types
+    // ➤ Statements
+    // ➤ Keywords
+    // ➤ Reserved words
+    // ➤ Operators
+    // ➤ Objects
+
+  // of the JavaScript language.
+
+  // The DOM is an API for accessing and manipulating the HTML document.
+    // It's implemented differently on a browser by browser basis though
+    // it's becoming more standardized lately.
+
+  // The BOM is an API for accessing and manipulating the browser window.
+    // It's implemented differently on a browser by browser basis though
+    // it's becoming more standardized lately.
+
+// JavaScript Version Numbers
+  // Vary browser by browser and so are mostly meaningless.
