62b96dff1c5467f44d3950029a26a4482bbd5a38
Ryan Linton <ryanlntn@gmail.com>
2013-04-22 00:06:33 -0700

    add to chapter 3

--- chapter3.js
+++ chapter3.js
@@ -1,3 +1,7 @@
 // Chapter 3
 // Language Basics
 
+// JavaScript is case sensitive
+// It's best practice to always use semicolons to end statements and braces to wrap condition blocks
+// Avoid using keywords and reserved words as identifiers or property names
+// Though JavaScript is loosely typed it's best to approach it as if it were static
