704aba6a712eaa4cc4d55023152477d04067a838
Ryan Linton <ryanlntn@gmail.com>
2013-05-01 23:37:38 -0700

    add to chapter 5

--- chapter5.js
+++ chapter5.js
@@ -1,2 +1,11 @@
 // Chapter 5
 // Reference Types
+
+// It’s recommended to use object literal notation only when you’re going to specify properties for readability.
+
+// Generally speaking, dot notation is preferred unless variables are necessary to access properties by name.
+
+// Arrays can contain a maximum of 4,294,967,295 items, which should be plenty for almost all programming needs.
+
+// It’s very important to understand the difference between a primitive Boolean value and a Boolean object
+// — it is recommended to never use the latter.
